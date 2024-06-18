a6fa9bd661179dc004e8b9d70a1455d813a7233e
Kunal Gosrani <kunalgosrani10@gmail.com>
2024-06-19 00:57:10 +0530

    feat: authentication with jwt

--- src/common/auth/access-token.ts
+++ src/common/auth/access-token.ts
@@ -1,11 +1,32 @@
+import { UnauthorizedException } from '@nestjs/common';
+import * as jwt from 'jsonwebtoken';
+
 export type JwtPayload = {
   userId: string;
 };
 
+const ISSUER = 'email-engine-backend-service';
+const SUBJECT = 'user-access-token';
+const SECRET = 'my-secret-key'; // should be stored in environment variable
+
 export function createAccessToken(payload: JwtPayload): string {
-  return payload.userId;
+  return jwt.sign(payload, SECRET, {
+    issuer: ISSUER,
+    expiresIn: `12h`,
+    subject: SUBJECT,
+  });
 }
 
 export function validateAccessToken(token: string): JwtPayload {
-  return { userId: token };
+  try {
+    return jwt.verify(token, SECRET, {
+      issuer: ISSUER,
+      subject: SUBJECT,
+    }) as JwtPayload;
+  } catch (err) {
+    if (err instanceof jwt.TokenExpiredError) {
+      throw new UnauthorizedException('Access token expired');
+    }
+    throw err;
+  }
 }
